107d1b9d84ba0d3e79a63f0c616227cf115e07f0
Fredrik Tibbling <50440682+codebyfredrik@users.noreply.github.com>
2020-07-09 22:51:24 +0200

    Update logic, structure and style (#7)

---
+++ src/components/Date.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import moment from 'moment';
+
+export const StyledDate = styled.li`
+  list-style-type: none;
+  font-weight: bold;
+  color: ${({ theme }) => theme.text};
+  transition: color 150ms ease-in;
+`;
+
+export const Date = ({ date }) => {
+  const rawDate = moment(date);
+  const today = moment().endOf('day');
+  const tomorrow = moment().add(1, 'day').endOf('day');
+  let phrase = '';
+
+  if (rawDate < today) phrase = 'Today, ';
+  if (rawDate < tomorrow) phrase = 'Tomorrow, ';
+
+  return <StyledDate>{`${phrase}${rawDate.format('D MMMM')}`}</StyledDate>;
+};
