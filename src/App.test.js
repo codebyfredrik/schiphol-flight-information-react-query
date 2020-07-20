7c1a492cf790528c43e57aa78c13843df1375c27
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-07-20 16:10:00 +0200

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react';
+import { App } from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders App component', () => {
+    render(<App />);
+  });
+  test('Header title and sub-title are visible', () => {
+    render(<App />);
+    expect(screen.getByText('Amsterdam Schipol Airport')).toBeInTheDocument();
+    expect(screen.getByText('Flight Information')).toBeInTheDocument();
+  });
+  test('Previous page button', () => {
+    render(<App />);
+    expect(screen.getByText('Previous page')).toBeInTheDocument();
+    expect(screen.getByText('Previous page')).toBeDisabled();
+  });
+  test('Next page button', () => {
+    render(<App />);
+    expect(screen.getByText('Next page')).toBeInTheDocument();
+  });
+  test('Filter button', () => {
+    render(<App />);
+    expect(screen.getByText('Filter')).toBeInTheDocument();
+  });
+  test('Theme button', () => {
+    render(<App />);
+    expect(screen.getByText('Dark theme')).toBeInTheDocument();
+  });
 });
