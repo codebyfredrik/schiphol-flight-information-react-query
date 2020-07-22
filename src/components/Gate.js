73d92463fe8d86c26db4661bb94c57e29295f1ce
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-07-22 15:05:42 +0200

    Update style

--- src/components/Gate.js
+++ src/components/Gate.js
@@ -2,30 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const StyledGate = styled.span`
-  display: inline-block;
+const StyledGate = styled.div`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
   position: relative;
   text-decoration: none;
-`;
-
-const GateNumber = styled.span`
-  display: inline-block;
   background-color: #ffd700;
   color: #262b2f;
-  line-height: 1;
-  margin-bottom: 1px;
+  font-size: 14px;
+  line-height: 14px;
   padding: 7px 9px;
-  position: relative;
   border-radius: 4px;
   font-weight: bold;
 `;
 
 const Gate = ({ gate, className }) => {
-  return (
-    <StyledGate className={className}>
-      <GateNumber>{gate}</GateNumber>
-    </StyledGate>
-  );
+  return <StyledGate className={className}>{gate}</StyledGate>;
 };
 
 Gate.propTypes = {
