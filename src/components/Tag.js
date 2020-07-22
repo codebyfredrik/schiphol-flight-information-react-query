e1633bc15e0427208f35e5fc67c0f77d8e1e74d0
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-07-22 14:54:41 +0200

    Update style

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -2,28 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const StyledTag = styled.span`
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  font-weight: bold;
-  padding: 0.1rem 0.3rem;
+const StyledTag = styled.div`
+  display: inline-block;
   border-radius: 4px;
+  font-weight: bold;
+  padding: 4px 5px;
   text-transform: uppercase;
-  font-size: 0.8rem;
-  line-height: 0.8rem;
+  font-size: 12px;
+  line-height: 10px;
   color: ${({ theme }) => theme.textTag};
   transition: color var(--transition-time) ease-in;
 `;
 
-const Label = styled.span``;
-
 const Tag = ({ label, className }) => {
-  return (
-    <StyledTag className={className}>
-      <span>{label}</span>
-    </StyledTag>
-  );
+  return <StyledTag className={className}>{label}</StyledTag>;
 };
 
 Tag.propTypes = {
