f7a9ada3da911d5226366a4afde35dacb83d23de
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-07-25 19:06:38 +0200

    Update style

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -31,7 +31,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   :root {
-  --transition-time: 200ms;
+  --transition-time: 150ms;
   --min-tap-target-height: 48px;
   --container-margin: 1.5rem;
     
