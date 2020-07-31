3e160302c81c802b9f0d69e710fc0b4e6b91a753
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-08-01 00:55:43 +0200

    Update style

--- src/App.js
+++ src/App.js
@@ -107,6 +107,7 @@ const Spacer = styled.div`
 
 const StyledButton = styled(Button)`
   color: ${({ theme }) => theme.colors.text};
+  border: 1px solid ${({ theme }) => theme.colors.borderButton};
 
   &:disabled {
     cursor: default;
