2006a78f7781afd15b2d314880899b367266abb4
Fredrik Tibbling <50440682+codebyfredrik@users.noreply.github.com>
2020-08-03 20:40:35 +0200

    Keypress hook (#39)
    * Add custom hook
    
    * Implement hook to close overlay

---
+++ src/hooks/useKeyPress.js
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+
+const useKeyPress = (key, action) => {
+  useEffect(() => {
+    const onKeyup = (e) => {
+      if (e.key === key) action();
+    };
+    window.addEventListener('keyup', onKeyup);
+    return () => window.removeEventListener('keyup', onKeyup);
+  }, []);
+};
+
+export { useKeyPress };
