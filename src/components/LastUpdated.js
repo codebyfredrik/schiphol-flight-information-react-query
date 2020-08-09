609f8c68ea244850da6aa9336178fe1ee822ad88
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-08-09 15:12:49 +0200

    Update style

--- src/components/LastUpdated.js
+++ src/components/LastUpdated.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useFormatTime } from './../hooks/index';
 
-const LastUpdated = ({ timestamp }) => {
+const StyledLastUpdated = styled.span`
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+const LastUpdated = ({ timestamp, className }) => {
   const { formattedTimestamp } = useFormatTime(timestamp, 'HH:mm');
-  return <span>{`Last updated ${formattedTimestamp}`}</span>;
+  return (
+    <StyledLastUpdated
+      className={className}
+    >{`Last updated ${formattedTimestamp}`}</StyledLastUpdated>
+  );
 };
 
 export { LastUpdated };
