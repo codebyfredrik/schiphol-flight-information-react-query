2a8e19b1452ba030a78c4b71117c37a834876d13
Fredrik Tibbling <50440682+codebyfredrik@users.noreply.github.com>
2020-08-10 17:19:25 +0200

    Refactor logic, structure and style (#45)

--- src/App.js
+++ src/App.js
@@ -1,75 +1,16 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { useToggle, useDarkMode } from './hooks/index';
 import { Theme } from './components/Theme';
 import { ReactQueryDevtools } from 'react-query-devtools';
 import { GlobalStyle } from './components/GlobalStyle';
+import { Header } from './components/Header';
 import { Overlay } from './components/Overlay';
 import {
   FlightsView,
   FlightDepartureView,
   FlightArrivalView,
 } from './views/index';
-
-const HeaderContainer = styled.div`
-  max-width: 1000px;
-  margin: auto;
-  padding: 1.5rem 1rem;
-`;
-
-const Header = styled.header`
-  background: linear-gradient(to right, #073590, #0d49c0);
-  border-bottom: 2px solid ${({ theme }) => theme.colors.yellow};
-`;
-
-const Title = styled.h1`
-  display: inline-block;
-  margin: 0;
-  font-size: 1.5rem;
-  line-height: 1.5rem;
-  font-family: 'Source Sans Pro', sans-serif;
-  color: ${({ theme }) => theme.colors.yellow};
-
-  @media screen and (min-width: 380px) {
-    font-size: 2rem;
-    line-height: 2rem;
-  }
-
-  @media screen and (min-width: 520px) {
-    font-size: 2.5rem;
-    line-height: 2.5rem;
-  }
-
-  @media screen and (prefers-reduced-motion: no-preference) {
-    transition: color var(--transition-time) ease-in;
-  }
-`;
-
-const SubTitle = styled.h3`
-  display: inline-block;
-  margin: 0;
-  font-size: 1rem;
-  line-height: 1rem;
-  margin-top: 0.25rem;
-  letter-spacing: 0.25rem;
-  font-family: 'Source Sans Pro', sans-serif;
-  color: ${({ theme }) => theme.colors.subTitle};
-
-  @media screen and (min-width: 520px) {
-    font-size: 1.25rem;
-    line-height: 1.25rem;
-  }
-
-  @media screen and (prefers-reduced-motion: no-preference) {
-    transition: color var(--transition-time) ease-in;
-  }
-`;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  -webkit-tap-highlight-color: transparent;
-`;
 
 const App = () => {
   const [page, setPage] = useState(0);
@@ -82,18 +23,7 @@ const App = () => {
       <>
         <ReactQueryDevtools initialIsOpen={false} />
         <GlobalStyle />
-        <Header>
-          <HeaderContainer>
-            <div>
-              <StyledLink to="/">
-                <Title>Amsterdam Schipol Airport</Title>
-              </StyledLink>
-            </div>
-            <StyledLink to="/">
-              <SubTitle>Flight Information</SubTitle>
-            </StyledLink>
-          </HeaderContainer>
-        </Header>
+        <Header />
         {overlayIsVisible && (
           <Overlay
             setFlightDirection={setFlightDirection}
