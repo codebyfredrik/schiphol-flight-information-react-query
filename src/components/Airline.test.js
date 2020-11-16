c0179817f75e7ba8aebb15935191158e7338730c
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-11-16 17:45:55 +0100

    Update test

--- src/components/Airline.test.js
+++ src/components/Airline.test.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { screen, waitForElementToBeRemoved } from '@testing-library/react';
 import { render } from './../utils/helpers/index';
+import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { handlers } from '../test/server-handlers';
 import { Airline } from './Airline';
@@ -21,4 +22,23 @@ describe('<Airline />', () => {
     await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
     expect(screen.getByText(/klm/i)).toBeInTheDocument();
   });
+  // test('Airline not found', async () => {
+  //   server.use(
+  //     rest.get(
+  //       `${process.env.REACT_APP_API_BASE_URL}/airlines/:prefixICAO`,
+  //       (req, res, ctx) => {
+  //         if (!req.params.prefixICAO) {
+  //           return res(
+  //             ctx.delay(0),
+  //             ctx.status(400),
+  //             ctx.json({ error: 'No airline with that prefix' })
+  //           );
+  //         }
+  //       }
+  //     )
+  //   );
+  //   render(<Airline prefixICAO="" />);
+
+  //   await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+  // });
 });
