bdf61a7d47c89301adb1d7062af284b25ca5bd52
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-11-21 22:22:03 +0100

    Update logic, structure and style

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,15 +1,28 @@
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { Emoji } from '../styles/styles';
+
+const Message = styled.span`
+  display: inline-block;
+  color: ${({ theme }) => theme.colors.text};
+  line-height: 0.8;
+`;
+
+const StyledError = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
 
 const Error = ({ message = 'no match', url = '/' }) => {
   return (
-    <div>
-      <p>
-        <span role="img" aria-label="screaming in fear">
-          😱
-        </span>
+    <StyledError>
+      <Emoji mr={10} role="img" aria-label="screaming in fear">
+        😱
+      </Emoji>
+      <Message>
         Oh no...{message}. Display <Link to={url}>all flights.</Link>
-      </p>
-    </div>
+      </Message>
+    </StyledError>
   );
 };
 
